7e68b8c27133281e5124f1675b7fce2aa37d6801
dilmurod <dilmurod8829@gmail.com>
2022-11-29 22:40:59 +0200

    homework

--- scripts/app.js
+++ scripts/app.js
@@ -5,8 +5,10 @@ setInterval(() => {
   let src = imageElement.getAttribute("src");
   if (src === "images/ochiq.png") {
     imageElement.setAttribute("src", "images/yoniq.png");
+    document.body.style.backgroundColor = "white";
   } else {
     imageElement.setAttribute("src", "images/ochiq.png");
+    document.body.style.backgroundColor = "black";
   }
 }, 2000);
 
